f9defd7a88a133fa83ff081b08a2572ca3e4bb43
Alex Kruglov <lime4512@mail.ru>
2023-12-01 02:56:14 +0300

    add style filmDetails

--- src/components/FilmsDetails/FilmDetails.jsx
+++ src/components/FilmsDetails/FilmDetails.jsx
@@ -11,12 +11,16 @@ export const FilmDetails = ({
 	slogan,
 	genres,
 	description,
+	ratingI,
+	ratingK,
 }) => {
 	const [genre, setGenre] = useState([])
+	const [country, serCountry] = useState([])
 	useEffect(() => {
 		setGenre(genres)
-	}, [genres])
-	console.log(genre)
+		serCountry(countries)
+	}, [genres, countries])
+
 	return (
 		<div className='filmDetails-container'>
 			<div className='film-content'>
@@ -28,33 +32,72 @@ export const FilmDetails = ({
 							alt='Img'
 							fill={true}
 							unoptimized={true}
-							className='imgTotal-card'
+							className='imgTotal-film'
 						/>
 					)}
 				</div>
 				<div className='film-text'>
-					<h1 className='film-title-ru'>{nameRu}</h1>
-					<p className='film-title-original'>{nameO}</p>
-					<h2>Описание</h2>
-					<p>Год производства: {year}</p>
+					<h1 className='film-title-ru'>
+						{nameRu == undefined ? nameO : nameRu}
+					</h1>
+					<p className='film-title-original'>
+						{nameRu == undefined ? <></> : nameO}
+					</p>
+					<h2 className='film-opisanie'>Описание</h2>
+					<p className='film-year'>
+						<span className='gray'>Год производства:</span> {year}
+					</p>
 
 					<ul className='film-genres'>
-						<li>Жанры:</li>
+						<li className='gray'>Жанры:</li>
 						{genre == undefined ? (
-							<p>Нет</p>
+							<p>-</p>
 						) : (
 							genre.map(item => <li key={Math.random()}>{item.genre}</li>)
 						)}
 					</ul>
 
-					<p>Страна: </p>
-					<p>Возрастное ограничение: {AgeLimits}</p>
-					<p>Слоган:{slogan}</p>
+					<ul className='film-countries'>
+						<li className='gray'>Страны:</li>
+						{country == undefined ? (
+							<p>-</p>
+						) : (
+							country.map(item => <li key={Math.random()}>{item.country}</li>)
+						)}
+					</ul>
+					<div className='film-ageLimits'>
+						<p className='film-ageLimits-title gray'>
+							Возрастное ограничение:{' '}
+						</p>
+						{AgeLimits == undefined ? (
+							<span>-</span>
+						) : (
+							AgeLimits.replace('age', '') + '+'
+						)}
+					</div>
+					<div className='film-slogan'>
+						<p className='film-slogan-title gray'>Слоган:</p>
+						{slogan == undefined ? <span>-</span> : slogan}
+					</div>
+
+					<h1 className='film-rating-title'>Рейтинг</h1>
+					<div className='film-rating'>
+						<div className='rating-imdb'>
+							<span className='gray'>Imb: </span>
+							{ratingI == undefined ? <span>-</span> : ratingI}
+						</div>
+						<div className='rating-kinopoisk'>
+							<span className='gray'>Kinopoisk: </span>
+							{ratingK == undefined ? <span>-</span> : ratingK}
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className='film-description'>
-				<h3>Сюжет</h3>
-				<p>{description}</p>
+				<h3 className='film-plot'>Сюжет</h3>
+				<p className='film-plot-text'>
+					{description == undefined ? <span>-</span> : description}
+				</p>
 			</div>
 		</div>
 	)
