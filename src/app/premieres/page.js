d0d996fa9ffe5f27db6eac2e04b49f1bd8728115
Alex Kruglov <lime4512@mail.ru>
2023-12-04 22:18:31 +0300

    add premieres

--- src/app/premieres/page.js
+++ src/app/premieres/page.js
@@ -3,7 +3,7 @@ import { Container } from '@/components/container/Container'
 import { Selects } from '@/components/Selects/Selects'
 import { useState, useEffect } from 'react'
 import { Card } from '@/components/Card/Card'
-
+import '@/style/release.scss'
 export default function Premieres() {
 	const [year, setYears] = useState('2023')
 	const [month, setMonths] = useState('JANUARY')
@@ -37,11 +37,32 @@ export default function Premieres() {
 		}
 		releaseMovie()
 	}, [year, month])
-	console.log(data)
 	return (
 		<Container>
 			<h1>Кинопремьеры</h1>
 			<Selects setYear={handleYear} setMonth={handleMonth} />
+
+			<div className='release-content'>
+				{data == undefined ? (
+					<h1>Релизов нет</h1>
+				) : data.length == 0 ? (
+					<h1>Релизов не найдено</h1>
+				) : (
+					<ul className='cardList'>
+						{data.map(item => (
+							<li key={Math.random()} className='cardList-item'>
+								<Card
+									titleRu={item.nameRu}
+									titleEn={item.nameEu}
+									img={item.posterUrlPreview}
+									ratingImdb={item.rating}
+									id={item.kinopoiskId}
+								/>
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
 		</Container>
 	)
 }
